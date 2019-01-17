17c044d02fd09bd511be494182d881410a7e6253
Eric Zhou <42453465+TheZooo@users.noreply.github.com>
2019-01-17 13:34:49 -1000

    Update foodShopFunc.js

--- foodShopFunc.js
+++ foodShopFunc.js
@@ -1,21 +1,22 @@
 var d = document;
 var myData;
-var amtBox = d.getElementsByTagName('div').length;
+var amtBox = d.getElementsByClassName('itemBox');
+var i = 0;
 
 function startA() {
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) { //4 = ready ; 200 = okay
       myData = JSON.parse(this.responseText);
-      window.alert(myData);
       }
     };
   xmlhttp.open("GET", "sample.json", true);
   xmlhttp.send();
   
-  window.alert(amtBox);
-  window.alert(myData);
   
-  //for (i = 0;)
-  //d.getElementsByClassName('itemDesc')
+  for (i; i < amtBox.length; i++) {
+    d.getElementsByClassName('itemDesc')[i].innerHTML = myData.food[i].desc;
+    window.alert(i);
+  }
+  
 }
